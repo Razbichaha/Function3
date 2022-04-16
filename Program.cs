4d6db3904fce589ea9f3d4aee6f0ef08b57be01b
Сергей <seregabur@live.ru>
2022-04-16 13:52:17 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Function3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputPlaer = 0;
+
+            OutputMenu(out inputPlaer);
+            
+            Console.Write("Вы  ввели число - " + inputPlaer);
+            Console.ReadKey();
+        }
+
+        static void OutputMenu(out int inputPlaer )
+        {
+            inputPlaer = 0;
+            bool thisIsInputOfValue = true;
+
+            while (thisIsInputOfValue)
+            {
+                Console.Write("Введите число ");
+                string stringTemp = Console.ReadLine();
+
+                if(int.TryParse(stringTemp, out inputPlaer))
+                {
+                    thisIsInputOfValue = false;
+                }else
+                {
+                    stringTemp = "";
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
